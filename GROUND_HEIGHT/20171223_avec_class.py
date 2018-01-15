d5bf127c1ca5e549a18ead7729c5c0e7ecf3f2dc
VicomtedeJouy <34004724+VicomtedeJouy@users.noreply.github.com>
2018-01-15 22:24:32 +0900

    Stable for ground order
    Re-order grounds in case of a misplaced ID nb
    Careful : work for only one building ! Hqve to separate them

--- GROUND_HEIGHT/20171223_avec_class.py
+++ GROUND_HEIGHT/20171223_avec_class.py
@@ -5,12 +5,15 @@ Created on Sat Dec 23 18:49:58 2017
 @author: Augustin
 """
 
+
 import clr
 clr.AddReference('ProtoGeometry')
 clr.AddReference('RevitNodes')
 clr.AddReference('DSCoreNodes')
+clr.AddReference('RevitAPIUI')
 
 from Autodesk.DesignScript.Geometry import *
+from Autodesk.Revit.UI import TaskDialog
 from Revit import *
 from DSCore import *
 
@@ -23,43 +26,50 @@ choice_value = IN[3]
  
 """ FUNCTIONS """
 def sort_list(test_list):
+     #sort the list according to the avg height of the floor
+	a = len(test_list)
+	msg = str(a)
+	msgBox = TaskDialog
+	msgBox.Show('title', msg)
+	
 	sorted_list = []
 	while len(test_list) > 1:
-		min = test_list[0]
-		index_min = 0
+		min = test_list[0][1]
 		for item in test_list:
-			if item < min:
+			if item[1] < min:
 				min = item
-				index_min = test_list.index(item)
-		sorted_list.append(min)
-		test_list.pop(index_min)
-	sorted_list.append(test_list[0])
+		sorted_list.append(min[0])
+		test_list.remove(min)
+	sorted_list.append(test_list[0][0])
 	return sorted_list
 	
 def remove_duplicates(test_list):
+     #remove duplicates and make an average height for each floors
 	result = []
-	duplicate = []
 	for item in test_list:
-		if item not in duplicate:
-			duplicate.append(item)
-			result.append(item)
+          avg = 0
+          test_value = item[0]
+          new_item = [test_value]
+          for item_test in test_list:
+               if item_test[0] == test_value:
+                    avg += item_test[1]
+                    test_list.remove(item_test)
+          new_item.append(avg)
+          result.append(new_item)
 	return result
- 
-def organise(list_test):
-    list_def = [list_test[0]]
-    for new_elem in list_test:
-    	id_1 = new_elem[0]
-    	id_2 = new_elem[1]
-    	for elem in list_def:
-    		if id_1 == elem[0] and id_2 == elem[1]:
-    			break
-    		list_def.append(elem)
-    return list_def
+    
+def is_in_list(elem, list_test):
+	if len(list_test) == 0:
+           return False
+	for elem_test in list_test:
+		if elem == elem_test:
+			return True
+	return False
  
 def build_level_list(data):
-    niv = [data[i].GetParameterValueByName('Niveau') for i in range(len(data))]
+    niv = [[data[i].GetParameterValueByName('Niveau'), BoundingBox.ByGeometry(data[i].Geometry()[0]).MinPoint.Z] for i in range(len(data))]
     # builds the groundList by height
-    level_list = [niv[i].Id for i in range(len(niv))]
+    level_list = [[niv[i][0].Id, niv[i][1]] for i in range(len(niv))]
     level_list = remove_duplicates(level_list)
     level_list = sort_list(level_list)
     return level_list
@@ -70,25 +80,23 @@ def build_ground_list(data, level_list):
         list_grounds.append(Ground(element, level_list))
     return list_grounds
           
-def build_list_results(list_grounds):
+def build_lists(list_grounds):
     list_results = []
-    temp = []
+    list_problems = []
     for ground in list_grounds:
-        ground.get_results(list_grounds)
-        temp.append(ground.list_results)
-        for i in range(len(temp)):
-            list_results.append(temp[i])
-    list_results = organise(list_results)
-    return list_results
+    	ground.get_results(list_grounds)
+        for module in ground.list_results:
+            if not is_in_list(module, list_results):
+                list_results.append(module)
+                if module.problem == True:
+            		list_problems.append(module) 
+    return list_results, list_problems
     
-def build_list_problems(list_results):
-    list_problems= []
-    for elem in list_results:
-        if elem[2] > max_height or elem[2] < min_height:
-            list_problems.append(elem)
-    return list_problems
+def results_display(list_results):
+    return [[module.id, module.level_info, module.distance] for module in list_results]
     
-        
+def problems_display(list_problems):
+	return[[module.id, module.problem_type, module.level_info, module.geometry] for module in list_problems]
             
 
  
@@ -97,6 +105,7 @@ class Ground:
         self.geometry = ground.Geometry()[0]
         self.id_number = ground.Id
         self.level_index = self.level_index_ground(ground, level_list)
+        self.level_name = self.level_name_ground(ground)
         self.point_down = BoundingBox.ByGeometry(self.geometry).MinPoint
         self.point_up = BoundingBox.ByGeometry(self.geometry).MaxPoint
         self.pseudo_geometry = self.geometry.Translate(Vector.ByCoordinates(0., 0., -self.point_down.Z))
@@ -104,19 +113,18 @@ class Ground:
     def level_index_ground(self, ground, level_list):
         return level_list.index(ground.GetParameterValueByName('Niveau').Id)
         
+    def level_name_ground(self, ground):
+        return ground.GetParameterValueByName('Niveau').Name
+        
     def get_results(self, list_grounds):
         self.list_results = []
         for ground in list_grounds:
             temp_1 = []
             temp_2 = []
             if ground.level_index == self.level_index-1 and self.does_superpose(ground):
-                temp_1.append(ground)
-                dist = self.distance_to(ground)
-                self.list_results.append([ground.id_number, self.id_number, dist, self.geometry, ground.geometry])
+                self.list_results.append(DistModule(ground, self))
             if ground.level_index == self.level_index+1 and self.does_superpose(ground):
-                temp_2.append(ground)
-                dist = self.distance_to(ground)
-                self.list_results.append([self.id_number, ground.id_number, dist, self.geometry, ground.geometry])
+                self.list_results.append(DistModule(self, ground))
         
     def does_superpose(self, ground):
         if Geometry.DoesIntersect(self.pseudo_geometry, ground.pseudo_geometry):
@@ -130,27 +138,56 @@ class Ground:
         dist2 = abs(self.point_up.Z - ground.point_down.Z)
         return min(dist1, dist2)
         
-    def get_results_old(self):
-        self.results = []
-        if len(self.surrounding_grounds[0]) > 0:
-            for ground in self.surrounding_grounds:
-                dist = self.distance_to(ground)
-                self.results.append([self.id_number, ground.id_number, dist, self.geometry, ground.geometry])
-        if len(self.surrounding_grounds[1]) > 0:
-            for ground in self.surrounding_grounds:
-                dist = self.distance_to(ground)
-                self.results.append([min(self.id_number, ground.id_number), max(self.id_number, ground.id_number), dist, ground.geometry, self.geometry])
         
         
-list_level = build_level_list(list_data)
+class DistModule:
+    def __init__(self, ground1, ground2):
+        self.id = (ground1.id_number, ground2.id_number)
+        self.level_info = ground1.level_name + " to " + ground2.level_name
+        self.distance = self.set_distance(ground1, ground2)
+        self.geometry = (ground1.geometry, ground2.geometry)
+        self.problem = self.set_state()
+        self.problem_type = self.set_pb_type()
+		
+    def __eq__(self, other):
+        if self.id[0] == other.id[0] and self.id[1] == other.id[1]:
+            return True
+        if self.id[0] == other.id[1] and self.id[1] == other.id[0]:
+            return True
+        return False
+		
+    def set_distance(self, ground1, ground2):
+        dist1 = abs(ground1.point_down.Z - ground2.point_up.Z) / 1000
+        dist2 = abs(ground1.point_up.Z - ground2.point_down.Z) / 1000
+        return min(dist1, dist2)
+        
+    def set_state(self):
+        if self.distance > max_height or self.distance < min_height:
+            return True
+        return False
+        
+    def set_pb_type(self):
+        if self.distance > max_height:
+            return "Trop haut : " + repr(self.distance)
+        if self.distance < min_height:
+            return "Trop bas : " + repr(self.distance)
+        return "Aucun"
+    
+        
 
-list_grounds = build_ground_list(list_data, list_level)
 
-list_results = build_list_results(list_grounds)
+def main(list_data):       
+    list_level = build_level_list(list_data)
 
-list_problems = build_list_problems(list_results)
+    list_grounds = build_ground_list(list_data, list_level)
 
-if choice_value == "Résultats généraux":
-    OUT = list_results
-if choice_value == "Listes des problèmes":
-    OUT = list_problems
+    list_results, list_problems = build_lists(list_grounds)
+
+    if choice_value == "Résultats généraux":
+        OUT = results_display(list_results)
+    if choice_value == "Liste des problèmes":
+        OUT = problems_display(list_problems)
+        
+    return OUT
+    
+OUT = main(list_data)
